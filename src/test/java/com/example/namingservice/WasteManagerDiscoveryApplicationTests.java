847806e8f52f78e75f13d41043b8ab65cab016cf
dreamerP <patriciacastelo99@gmail.com>
2024-04-14 18:51:10 -0400

    Init commit

---
+++ src/test/java/com/example/namingservice/WasteManagerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.namingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteManagerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
